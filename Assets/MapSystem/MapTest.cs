89eb22114c6781027e27cc4e4334f0d185a360a8
laguzfable <78677416+laguzfable@users.noreply.github.com>
2025-09-07 16:08:53 +0800

    各種功能開始正常 準備上正式劇本

--- Assets/MapSystem/MapTest.cs
+++ Assets/MapSystem/MapTest.cs
@@ -1,13 +1,37 @@
 ﻿using UnityEngine;
+using Naninovel;
+using Naninovel.UI;
 
 public class MapTest : MonoBehaviour
 {
+    [SerializeField] private Camera mapCamera;
+
     void Start()
     {
         // 直接從 PlayerPrefs 讀，預設 1=白天
         bool isDay = PlayerPrefs.GetInt("MapIsDay", 1) == 1;
 
         Debug.Log("[MapTest] 啟動 MapTest，isDay=" + isDay);
+
+        Engine.GetService<IUIManager>().SetUIVisibleWithToggle(false);
+
+        var scriptPlayer = Engine.GetService<IScriptPlayer>();
+        scriptPlayer.Stop();
+
+        var continueUI = Object.FindObjectOfType<ContinueInputUI>();
+        if (continueUI != null)
+            continueUI.Visible = false;
+
+        var printerMgr = Engine.GetService<ITextPrinterManager>();
+        var dialoguePrinter = printerMgr.GetActor(printerMgr.DefaultPrinterId);
+        if (dialoguePrinter != null)
+            dialoguePrinter.Visible = false;
+
+        var naniCamera = Engine.GetService<ICameraManager>().Camera;
+        naniCamera.enabled = false;
+
+        if (mapCamera != null)
+            mapCamera.enabled = true;
 
         var bg = FindObjectOfType<MapBackgroundController>();
         if (bg != null)
